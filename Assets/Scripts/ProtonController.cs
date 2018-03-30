a4900b62135f850f1cf0904037aa475a6a1a52d9
Zak-Olyarnik <zwo24@drexel.edu>
2018-03-30 01:15:52 -0400

    Update and reflection bug fix for website

--- Assets/Scripts/ProtonController.cs
+++ Assets/Scripts/ProtonController.cs
@@ -7,7 +7,7 @@ public class ProtonController : MonoBehaviour
 	public static float speed = 1f;			// movement speed
 
 	private Rigidbody2D rb;
-	private Vector3 target = new Vector3(0, 0, 0);		// move towards center
+	private Vector3 target = Vector3.zero;      		// move towards center
 	private Vector2 movement;							// movement direction
 	private float rotAngle;								// direction of sprite
 	private bool redirect = false;						// whether proton has ricochetted off wire yet
@@ -21,34 +21,24 @@ public class ProtonController : MonoBehaviour
 		if (!redirect)
 		{
 			movement = (target - transform.position).normalized;
-			transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed); }
-		else
-		{ rb.velocity = movement * speed; }
+        }
 
-		// set sprite to face direction of movement
-		rotAngle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg + 90f;
+        rb.velocity = movement * speed;
+
+        // set sprite to face direction of movement
+        rotAngle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg + 90f;
 		transform.rotation = Quaternion.AngleAxis(rotAngle, Vector3.forward);
 	}
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
-		// different ricochet behavior based on which part of the circuit border was hit
-		if (coll.gameObject.tag == "side_border")
-		{
-			movement = new Vector2(-movement.x, movement.y);
-			redirect = true;
-		}
-		else if (coll.gameObject.tag == "hz_border")
-		{
-			movement = new Vector2(movement.x, -movement.y);
-			redirect = true;
-		}
-		else if (coll.gameObject.tag == "electrify")
-		{
-			movement = new Vector2(-movement.x, -movement.y);
-			redirect = true;
-		}
-	}
+        if (coll.gameObject.tag == "reflect" || (coll.gameObject.tag == "electrify" && target == Vector3.zero))
+        {
+            Vector2 inNormal = coll.contacts[0].normal;
+            movement = Vector3.Reflect(movement.normalized, inNormal);
+            redirect = true;
+        }
+    }
 
 	void OnTriggerEnter2D(Collider2D coll)
 	{
